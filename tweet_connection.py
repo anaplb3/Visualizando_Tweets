0519d3d49b73fcf276df353c0f87498bebe2b34f
anaplb3 <ana.paula@dce.ufpb.br>
2019-05-17 12:11:58 -0300

    ajeitando uma parada aqui

--- tweet_connection.py
+++ tweet_connection.py
@@ -7,10 +7,10 @@ class TweetConnection:
         tweets = []
         try:
             conexao = TwitterSearch(
-                consumer_key = "kjJSuZiBHwNwY8BXg5ZFQRJ4y",
-                consumer_secret = "zp4bzoEzPoTTq61nyfWJ6SV66kWBkXuiB3KzxUYWwdlsq8KK34",
-                access_token = "1010185649448804353-ZaTEXJqt6w8jai16GtBjPgUUkhr7B0",
-                access_token_secret = "sIF1uVph85e8IyBcFhkhPQadZ0aF0ET06TqfChHSZ7P69"
+                consumer_key = "***",
+                consumer_secret = "***",
+                access_token = "***",
+                access_token_secret = "***"
             )
 
             cursor = TwitterSearchOrder()
